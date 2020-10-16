ac454779578e399d5a873fff9291ca8392f61f7d
Felipe França <contatofelipefranca12@gmail.com>
2020-10-16 20:12:53 -0300

    Adiciona a conclusão do exercício DoadorSangue

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace DoadorSangue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeUsuario;
+            string textoIdadeUsuario;
+            bool idadeUsuarioValida;
+
+            Console.Clear();
+            Console.WriteLine("--- Doador de sangue");
+
+            Console.Write("Digite sua idade: ");
+            textoIdadeUsuario = Console.ReadLine();
+
+            idadeUsuarioValida = int.TryParse(textoIdadeUsuario, out idadeUsuario);
+
+            if (!idadeUsuarioValida)
+            {
+                Console.WriteLine("Valor inválido");
+                Environment.Exit(-1);
+            }
+            else if (idadeUsuario >= 18 && idadeUsuario <= 67)
+            {
+                Console.WriteLine("Você pode ser doador de sangue.");
+            }
+            else if (idadeUsuario < 18)
+            {
+                Console.WriteLine("Você não pode ser doador de sangue.");
+            }
+            else if (idadeUsuario > 67)
+            {
+                Console.WriteLine("Você não pode ser doador de sangue.");
+            }
+        }
+    }
+}
